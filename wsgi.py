f09f98ee58e67ac9bbed1cb9ed1247008d9f1d63
CROWDMAGICIAN <CONTACT@COCOO.UK>
2024-09-18 21:10:29 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,4 @@
 from main import app
 
-if __name__ != "__main__":
-    # This block will be used when running the app with a WSGI server like Gunicorn or Waitress
-    application = app
-
+if __name__ == "__main__":
+    app.run()
